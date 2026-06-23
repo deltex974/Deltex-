0612ca2e19a459b323c30c4e728b0fdfc4332312
deltex974 <antoine.andre.77@gmail.com>
2026-06-23 23:54:37 +0200

    Update synthese.js

--- api/synthese.js
+++ api/synthese.js
@@ -26,7 +26,8 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
+        "Authorization": `Bearer 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`,
+
       },
       body: JSON.stringify({
         model: "llama3-8b-8192",
